7e381d22c90d1c9605b11bfb6014267f51667945
Puja <amir.dialehabady@uni-ulm.de>
2017-12-22 21:17:27 +0100

    added missing classes and first views

---
+++ Quartett/GameSettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameSettingsViewController.swift
+//  Quartett
+//
+//  Created by Puja Dialehabady on 22.12.17.
+//  Copyright © 2017 Mobile Application Lab. All rights reserved.
+//
+
+import UIKit
+
+class GameSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Quartett/Rounds.swift
@@ -0,0 +1,17 @@
+//
+//  Rounds.swift
+//  Quartett
+//
+//  Created by Puja Dialehabady on 22.12.17.
+//  Copyright © 2017 Mobile Application Lab. All rights reserved.
+//
+
+import Foundation
+
+class Rounds {
+    
+    var curRound = 0
+    var results: [Result] = []
+     
+    
+}
